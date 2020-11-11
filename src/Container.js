536407969fe03593d024ed9a8eae2c51c48cc8e5
Ibrahima Thiam <ibrahimathiam672@gmail.com>
2020-11-11 21:04:56 +0100

    fin

--- src/Container.js
+++ src/Container.js
@@ -1,38 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import images from './data/data';
 
 const Container = () => {
-    const dataCat = [];
-    const [stateDataCat, setDataCat] = useState('')
+
+    const [dataImage, setdataImaga] = useState(images)
+
+    const [stateUrl1, setStateUrl1] = useState(images[0].url)
+    const [stateUrl2, setStateUrl2] = useState(images[1].url)
+
+    const [stateScore1, setStateScore1] = useState(0)
+    const [stateScore2, setStateScore2] = useState(0)
 
 
-    useEffect(() => {
-        images.forEach((image) => dataCat.push({ score: 0, url: image.url, id: image.id }))
-        console.log(dataCat)
-        setDataCat(dataCat)
-        console.log(stateDataCat[0])
-    }, [])
+    const [stateId1, setStateId1] = useState(images[0].id)
+    const [stateId2, setStateId2] = useState(images[1].id)
+
+
+    const randomImg = (id) => {
+
+        const newData = images.map(image => {
+            image.score = 0;
+            return image;
+        })
+        setdataImaga(newData)
+
+        console.log(id)
+        let copyData = [...dataImage]
+        let index = copyData.findIndex(Element => Element.id === id)
+        console.log(copyData[index])
+
+        copyData[index] = { ...copyData[index], score: copyData[index].score + 1 }
+        console.log(copyData[index])
+        setdataImaga(copyData)
 
 
 
+        const urlRandom = Math.floor(Math.random() * copyData.length);
+        const url1 = copyData[urlRandom];
 
 
-
-
-    const randomImg = () => {
-        const urlRandom = Math.floor(Math.random() * images.length);
-        const url1 = images[urlRandom];
-
-        // console.log(images[urlRandom]);
-        // console.log(images[urlRandom].url);
-        // console.log(images[urlRandom].id);
-
-        const urlRandom1 = Math.floor(Math.random() * images.length);
-        const url2 = images[urlRandom1]
-
-        // console.log(images[urlRandom1]);
-        // console.log(images[urlRandom1].url);
-        // console.log(images[urlRandom1].id);
+        const urlRandom1 = Math.floor(Math.random() * copyData.length);
+        const url2 = copyData[urlRandom1];
 
         setStateUrl1(url1.url)
         setStateUrl2(url2.url)
@@ -40,17 +48,14 @@ const Container = () => {
         setStateId1(url1.id)
         setStateId2(url2.id)
 
+        setStateScore1(url1.score)
+        setStateScore2(url2.score)
+
+
+
+
     }
 
-
-
-    const [stateUrl1, setStateUrl1] = useState(images[0].url)
-    const [stateUrl2, setStateUrl2] = useState(images[1].url)
-
-    const [stateId1, setStateId1] = useState(images[0].id)
-    const [stateId2, setStateId2] = useState(images[1].id)
-
-    const [score, setScore] = useState(0)
 
     return (
 
@@ -59,12 +64,14 @@ const Container = () => {
             <div className="App-header">
                 <div style={{ display: "flex", cursor: "pointer" }}>
                     <div style={{ marginRight: 40 }}>
-                        <img src={stateUrl1} alt="logo" style={{ width: 140, height: 160 }} class="img-thumbnail" onClick={randomImg} />
-                        <p>Score: {stateId1}</p>
+                        <img src={stateUrl1} alt="logo" style={{ width: 140, height: 160 }} class="img-thumbnail" onClick={_ => randomImg(stateId1)} />
+                        <p>id: {stateId1}  </p>
+                        <p> sciore : {stateScore1}</p>
                     </div>
                     <div>
-                        <img src={stateUrl2} alt="logo" style={{ width: 140, height: 160 }} class="img-thumbnail" onClick={randomImg} />
-                        <p>Score: {stateId2} </p>
+                        <img src={stateUrl2} alt="logo" style={{ width: 140, height: 160 }} class="img-thumbnail" onClick={_ => randomImg(stateId2)} />
+                        <p>id: {stateId2}  </p>
+                        <p> sciore : {stateScore2}</p>
                     </div>
 
                 </div>
