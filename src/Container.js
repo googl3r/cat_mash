48ffcd9f73a41446ec784d8d523831bc03fedfd2
Ibrahima Thiam <ibrahimathiam672@gmail.com>
2020-11-12 11:13:28 +0100

    refactoring du code

--- src/Container.js
+++ src/Container.js
@@ -1,61 +1,45 @@
-import React, { useState } from 'react';
-import images from './data/data';
+import React, { useState, useEffect } from 'react';
+import data from './data/data';
 
 const Container = () => {
 
-    const [dataImage, setdataImaga] = useState(images)
+    const [dataCat, setDataCat] = useState(data);
+    const [cat1, setCat1] = useState('');
+    const [cat2, setCat2] = useState('');
 
-    const [stateUrl1, setStateUrl1] = useState(images[0].url)
-    const [stateUrl2, setStateUrl2] = useState(images[1].url)
-
-    const [stateScore1, setStateScore1] = useState(0)
-    const [stateScore2, setStateScore2] = useState(0)
-
-
-    const [stateId1, setStateId1] = useState(images[0].id)
-    const [stateId2, setStateId2] = useState(images[1].id)
-
-
-    const randomImg = (id) => {
-
-        const newData = images.map(image => {
+    useEffect(() => {
+        const newDataCat = data.map(image => {
             image.score = 0;
             return image;
         })
-        setdataImaga(newData)
+        setDataCat(newDataCat)
 
-        console.log(id)
-        let copyData = [...dataImage]
-        let index = copyData.findIndex(Element => Element.id === id)
-        console.log(copyData[index])
+        const indexCat1 = Math.floor(Math.random() * dataCat.length);
+        const indexCat2 = Math.floor(Math.random() * dataCat.length);
 
-        copyData[index] = { ...copyData[index], score: copyData[index].score + 1 }
-        console.log(copyData[index])
-        setdataImaga(copyData)
+        setCat1(newDataCat[indexCat1])
+        setCat2(newDataCat[indexCat2])
 
 
-
-        const urlRandom = Math.floor(Math.random() * copyData.length);
-        const url1 = copyData[urlRandom];
-
-
-        const urlRandom1 = Math.floor(Math.random() * copyData.length);
-        const url2 = copyData[urlRandom1];
-
-        setStateUrl1(url1.url)
-        setStateUrl2(url2.url)
-
-        setStateId1(url1.id)
-        setStateId2(url2.id)
-
-        setStateScore1(url1.score)
-        setStateScore2(url2.score)
+    }, [])
 
 
 
+    const getRandomCAtandScore = (id) => {
+        // console.log(id)
+        let copyDataCat = [...dataCat];
+        let index = copyDataCat.findIndex(Element => Element.id === id)
+        copyDataCat[index] = { ...copyDataCat[index], score: copyDataCat[index].score + 1 }
+        setDataCat(copyDataCat)
 
+        console.log(dataCat)
+
+        const indexCat1 = Math.floor(Math.random() * dataCat.length);
+        const indexCat2 = Math.floor(Math.random() * dataCat.length);
+
+        setCat1(dataCat[indexCat1])
+        setCat2(dataCat[indexCat2])
     }
-
 
     return (
 
@@ -64,14 +48,12 @@ const Container = () => {
             <div className="App-header">
                 <div style={{ display: "flex", cursor: "pointer" }}>
                     <div style={{ marginRight: 40 }}>
-                        <img src={stateUrl1} alt="logo" style={{ width: 140, height: 160 }} class="img-thumbnail" onClick={_ => randomImg(stateId1)} />
-                        <p>id: {stateId1}  </p>
-                        <p> sciore : {stateScore1}</p>
+                        <img src={cat1.url} alt="logo" style={{ width: 140, height: 160 }} class="img-thumbnail" onClick={_ => getRandomCAtandScore(cat1.id)} />
+                        <p> score: {cat1.score} </p>
                     </div>
                     <div>
-                        <img src={stateUrl2} alt="logo" style={{ width: 140, height: 160 }} class="img-thumbnail" onClick={_ => randomImg(stateId2)} />
-                        <p>id: {stateId2}  </p>
-                        <p> sciore : {stateScore2}</p>
+                        <img src={cat2.url} alt="logo" style={{ width: 140, height: 160 }} class="img-thumbnail" onClick={_ => getRandomCAtandScore(cat2.id)} />
+                        <p> score: {cat2.score} </p>
                     </div>
 
                 </div>
